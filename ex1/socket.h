af03a756af4378fb4a6457822ef30674dd404080
galhar <1234mayA>
2020-11-01 07:08:51 -0500

    socket files ready for implementation

--- ex1/socket.h
+++ ex1/socket.h
@@ -4,6 +4,7 @@
 
 #ifndef IOT_EXS_SOCKET_H
 #define IOT_EXS_SOCKET_H
+
 /**
  * Initializes the socket
  * Host is the destination address as DNS: en8wtnrvtnkt5.x.pipedream.net, or as IPv4: 35.169.0.97
@@ -16,17 +17,41 @@
 int SocketInit(char *host, char *port);
 
 
+/**
+ * Connects to the socket (establishes TCP connection to the pre-defined host and port)
+ * Returns 0 on success, -1 on failure
+ * @return
+ */
 int SocketConnect(void);
 
 
+/**
+ * Writes len bytes from payload buffer to the established connection
+ * Returns the number of bytes written on success, -1 on failure
+ * @param payload
+ * @param len
+ * @return
+ */
 int SocketWrite(unsigned char *payload, unsigned int len);
 
 
+/**
+ * Reads up to max_len bytes from the established connection to the provided buf buffer, for up to timeout_ms
+ * (doesn’t block longer than that, even if not all max_len bytes were received)
+ * Returns the number of bytes read on success, -1 on failure
+ * @param buf
+ * @param max_len
+ * @param timeout_ms
+ * @return
+ */
+int SocketRead(unsigned char *buf, unsigned int max_len, unsigned int timeout_ms);
 
-int SocketRead(unsigned char *buf, unsigned int max_len, unsigned int
-timeout_ms);
 
-
+/**
+ * Closes the established connection
+ * Returns 0 on success, -1 on failure
+ * @return
+ */
 int SocketClose();
 
 #endif //IOT_EXS_SOCKET_H
--- ex1/socket_linux.c
+++ ex1/socket_linux.c
@@ -2,3 +2,26 @@
 // Created by osboxes on 11/1/20.
 //
 
+int SocketInit(char *host, char *port) {
+
+}
+
+
+int SocketConnect(void) {
+
+}
+
+
+int SocketWrite(unsigned char *payload, unsigned int len) {
+
+}
+
+
+int SocketRead(unsigned char *buf, unsigned int max_len, unsigned int timeout_ms) {
+
+}
+
+
+int SocketClose() {
+
+}
