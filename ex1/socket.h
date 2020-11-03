8f0b55080a0d14810c31c6fc45026c2dba4b2bba
galhar <1234mayA>
2020-11-03 06:21:07 -0500

    socket files ready for implementation with little change ++more little change

--- ex1/socket.h
+++ ex1/socket.h
@@ -19,7 +19,7 @@ int SocketInit(char *host, char *port);
 
 /**
  * Connects to the socket (establishes TCP connection to the pre-defined host and port)
- * Returns 0 on success, -1 on failure
+ * Returns 0 on success, -1 on failure.
  * @return
  */
 int SocketConnect(void);
@@ -27,7 +27,7 @@ int SocketConnect(void);
 
 /**
  * Writes len bytes from payload buffer to the established connection
- * Returns the number of bytes written on success, -1 on failure
+ * Returns the number of bytes written on success, -1 on failure.
  * @param payload
  * @param len
  * @return
@@ -38,7 +38,7 @@ int SocketWrite(unsigned char *payload, unsigned int len);
 /**
  * Reads up to max_len bytes from the established connection to the provided buf buffer, for up to timeout_ms
  * (doesn’t block longer than that, even if not all max_len bytes were received)
- * Returns the number of bytes read on success, -1 on failure
+ * Returns the number of bytes read on success, -1 on failure.
  * @param buf
  * @param max_len
  * @param timeout_ms
@@ -49,7 +49,7 @@ int SocketRead(unsigned char *buf, unsigned int max_len, unsigned int timeout_ms
 
 /**
  * Closes the established connection
- * Returns 0 on success, -1 on failure
+ * Returns 0 on success, -1 on failure.
  * @return
  */
 int SocketClose();
